d71ce1192ae00611c5b51083ac47dc53677e9c21
Rafika Fitri <80154263+Rafikafitri11769@users.noreply.github.com>
2021-04-07 08:06:58 +0700

    Delete tugas5 directory

--- tugas5/setings.java
+++
@@ -1,8 +0,0 @@
-{
-    "editor.fontSize": 14,
-    "window.zoomLevel": -1,
-    "workbench.colorTheme": "Dracula",
-    "workbench.iconTheme": "vscode-icons",
-    "java.project.outputPath": "bin",
-    "editor.formatOnSave": true,
-}
